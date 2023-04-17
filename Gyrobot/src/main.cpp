2fce6c099a2a4804a6ab7f77766edefb6758d35e
doyvde <95855418+doyvde@users.noreply.github.com>
2023-04-17 15:10:02 +0200

    essai

--- Gyrobot/src/main.cpp
+++ Gyrobot/src/main.cpp
@@ -1,4 +1,27 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+// Timers
+unsigned long timer = 0;
+float timeStep = 0.01;
+
+// Pitch, Roll and Yaw values
+float pitch = 0;
+float roll = 0;
+float yaw = 0;
+
+
+int led = 13;
+int ena = 5;
+int enb = 6;
+int ina = 7;
+int inb = 4;
+int inc = 9;
+int ind = 8;
+
 
 void setup() {
   // put your setup code here, to run once:
